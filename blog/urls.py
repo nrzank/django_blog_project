66900c8b7276bbc7d39ddda732ab1d50ef0dc327
nurzhank <asapnrz@gmail.com>
2024-05-27 09:15:31 +0500

    22.05.2024 Django. MVT 1.

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_get),
+    path('<int:post_id>/', views.detail),
     path('comments/', views.comment)
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,10 +9,20 @@ from blog.models import Post, Comment
 
 
 def post_get(request, *args, **kwargs):
-    post = Post.objects.get(pk=8)
+    post_id = request.GET.get('post_id')
+    post = Post.objects.get(pk=post_id)
+    print(kwargs.GET)
+    print(post)
     return HttpResponse(f'<h1>{post.title}</h1>'
                         f'<p>{post.content}</p>')
 
+
+
+def detail(request, *args, **kwargs):
+    post_id = kwargs.GET.get('post_id')
+    post = Post.objects.get(pk=post_id)
+    return HttpResponse(f'<h1>{post.title}</h1>'
+                        f'<p>{post.content}</p>')
 
 
 def comment(request, *args, **kwargs):
